9c320331559529087127bfb920c28053d525832a
Ivan javier <ig6119838@gmail.com>
2025-03-31 22:51:01 -0300

    update day

--- main.js
+++ main.js
@@ -28,29 +28,44 @@ async function reloadTranslations() {
     translatePage(language, data);
 }
 
+const textColors = {
+    "databases":"Bases De Datos:",
+    "project-description": "App de veterinaria donde puede gestionar los pacientes contando con registro, login y recuperacion de clave",
+    "project2-description":"App de blog con registro, login y recuperacion de clave",
+    "project3-description":"Busca la temperatura del pais con su pais y capital",
+    "project4-description":"Convertir a los visitantes en clientes mediante compaña de marketing o publicidad",
+    "date":"2023 - Febrero",
+    "data2":"2023 - Agosto",
+    "data3":"2024 - Febrero",
+    "date4":"2024 - Septiembre"
+}
+
 function translatePage(language, data){
     let traduction = localStorage.getItem('traduction') || translate.textContent
-
     document.querySelectorAll("[data-key]").forEach((element) => {
-
         //Transicion
         element.classList.add("fade-out");
-
         setTimeout(()=> {
             const key = element.getAttribute("data-key");
             if (data[language][key]) {
-                element.textContent = data[language][key];
+                if (textColors[key]) {
+                    element.innerHTML = `<span class='colors'>${data[language][key]}</span>`;
+                    element.classList.remove("fade-out");
+                    element.classList.add("fade-in");
+                    setTimeout(()=> {
+                        element.classList.remove("fade-in");
+                    },600);
+                    return;
+                }
+                element.innerHTML = data[language][key];
             }
-
             element.classList.remove("fade-out");
             element.classList.add("fade-in");
-
             setTimeout(()=> {
                 element.classList.remove("fade-in");
             },600);
         },600)
     });
-
     if(traduction == "En"){
         translate.textContent = "Es"
         localStorage.setItem("traduction", "Es");
